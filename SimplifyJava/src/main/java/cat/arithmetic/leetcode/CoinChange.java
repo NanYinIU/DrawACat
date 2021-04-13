815c138ed7a778d82fbe827029952e7ca0f22210
高国兴 <nanyiniu76@gmail.com>
2021-04-13 20:30:32 +0800

    226. 翻转二叉树

---
+++ SimplifyJava/src/main/java/cat/arithmetic/leetcode/CoinChange.java
@@ -0,0 +1,58 @@
+package cat.arithmetic.leetcode;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ *
+ * 322. 零钱兑换
+ *
+ * 给定不同面额的硬币 coins 和一个总金额 amount。编写一个函数来计算可以凑成总金额所需的最少的硬币个数。如果没有任何一种硬币组合能组成总金额，返回 -1。
+ *
+ * 你可以认为每种硬币的数量是无限的。
+ *
+ *
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/coin-change
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author gaoguoxing
+ * @version 1.0
+ * @date 2021-04-12
+ */
+public class CoinChange {
+
+    public static void main(String[] args) {
+
+    }
+
+    int minResult = 0;
+
+    int[] dp ;
+
+    public int coinChange(int[] coins, int amount) {
+
+       dp = new int[amount];
+
+        minResult = amount;
+
+        return doCoin(coins,amount);
+    }
+
+    public int doCoin(int[] coins, int amount) {
+        // base case
+        dp[0] = 0;
+        // 外层 for 循环在遍历所有状态的所有取值
+        for (int i = 0; i < dp.length; i++) {
+            // 内层 for 循环在求所有选择的最小值
+            for (int coin : coins) {
+                // 子问题无解，跳过
+                if (i - coin < 0) continue;
+                dp[i] = Math.min(dp[i], 1 + dp[i - coin]);
+            }
+        }
+        return (dp[amount] == amount + 1) ? -1 : dp[amount];
+    }
+
+}

18d7eb5aeb1603dbe5389ac7780db7d5a46a5cd5
高国兴 <nanyiniu76@gmail.com>
2021-05-12 20:27:59 +0800

    todo

--- SimplifyJava/src/main/java/cat/arithmetic/leetcode/middle_96_numTrees/Solution.java
+++ SimplifyJava/src/main/java/cat/arithmetic/leetcode/middle_96_numTrees/Solution.java
@@ -31,12 +31,23 @@ import java.util.Stack;
 public class Solution {
 
 
+    /**
+     什么是二叉搜索树？
+     若它的左子树不空，则左子树上所有结点的值均小于它的根结点的值；
+     若它的右子树不空，则右子树上所有结点的值均大于它的根结点的值；
+     它的左、右子树也分别为二叉排序树
+     */
     public int numTrees(int n) {
-        int[] dp = new int[n];
-        return 0;
+        int[] G = new int[n + 1];
+        G[0] = 1;
+        G[1] = 1;
+
+        for (int i = 2; i <= n; ++i) {
+            for (int j = 1; j <= i; ++j) {
+                G[i] += G[j - 1] * G[i - j];
+            }
+        }
+        return G[n];
     }
 
-    int calcNumTrees(int start, int end) {
-        return 0;
-    }
 }

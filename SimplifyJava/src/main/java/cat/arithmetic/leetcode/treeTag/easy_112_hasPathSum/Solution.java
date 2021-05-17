56da59800c8074933b38b08c139e2aecee8a8d74
高国兴 <nanyiniu76@gmail.com>
2021-05-17 11:34:10 +0800

    112. 路径总和

---
+++ SimplifyJava/src/main/java/cat/arithmetic/leetcode/treeTag/easy_112_hasPathSum/Solution.java
@@ -0,0 +1,63 @@
+package cat.arithmetic.leetcode.treeTag.easy_112_hasPathSum;
+
+import cat.arithmetic.leetcode.treeTag.TreeNode;
+
+/**
+ * 112. 路径总和
+ *
+ * 给你二叉树的根节点 root 和一个表示目标和的整数 targetSum ，
+ * 判断该树中是否存在 根节点到叶子节点 的路径，
+ * 这条路径上所有节点值相加等于目标和 targetSum 。
+ *
+ * 叶子节点 是指没有子节点的节点。
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
+ * 输出：true
+ * 示例 2：
+ *
+ *
+ * 输入：root = [1,2,3], targetSum = 5
+ * 输出：false
+ * 示例 3：
+ *
+ * 输入：root = [1,2], targetSum = 0
+ * 输出：false
+ *  
+ *
+ * 提示：
+ *
+ * 树中节点的数目在范围 [0, 5000] 内
+ * -1000 <= Node.val <= 1000
+ * -1000 <= targetSum <= 1000
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/path-sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author gaoguoxing
+ * @version 1.0
+ * @date 2021-05-17
+ */
+public class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+        boolean has = false;
+        int nextTarget = targetSum - root.val;
+        if ((root.left == null || root.right == null) && nextTarget == 0) {
+            return true;
+        }
+        if (root.left != null) {
+            has = hasPathSum(root.left, nextTarget) || has;
+        }
+        if (root.right != null) {
+            has = hasPathSum(root.right, nextTarget) || has;
+        }
+        return has;
+    }
+}

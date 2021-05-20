5950caf1ce07345cf27a80479d6c994105ff4029
高国兴 <nanyiniu76@gmail.com>
2021-05-20 11:46:44 +0800

    173. 二叉搜索树迭代器

--- SimplifyJava/src/main/java/cat/arithmetic/leetcode/treeTag/middle_173_BSTIterator/BSTIterator.java
+++ SimplifyJava/src/main/java/cat/arithmetic/leetcode/treeTag/middle_173_BSTIterator/BSTIterator.java
@@ -2,11 +2,15 @@ package cat.arithmetic.leetcode.treeTag.middle_173_BSTIterator;
 
 import cat.arithmetic.leetcode.treeTag.TreeNode;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * 173. 二叉搜索树迭代器
  *
  * 实现一个二叉搜索树迭代器类BSTIterator ，表示一个按中序遍历二叉搜索树（BST）的迭代器：
- * BSTIterator(TreeNode root) 初始化 BSTIterator 类的一个对象。BST 的根节点 root 会作为构造函数的一部分给出。指针应初始化为一个不存在于 BST 中的数字，且该数字小于 BST 中的任何元素。
+ * BSTIterator(TreeNode root) 初始化 BSTIterator 类的一个对象。BST 的根节点 root 会作为构造函数的一部分给出。
+ * 指针应初始化为一个不存在于 BST 中的数字，且该数字小于 BST 中的任何元素。
  * boolean hasNext() 如果向指针右侧遍历存在数字，则返回 true ；否则返回 false 。
  * int next()将指针向右移动，然后返回指针处的数字。
  * 注意，指针初始化为一个不存在于 BST 中的数字，所以对 next() 的首次调用将返回 BST 中的最小元素。
@@ -60,15 +64,38 @@ import cat.arithmetic.leetcode.treeTag.TreeNode;
  */
 public class BSTIterator {
 
-    public BSTIterator(TreeNode root) {
+    private TreeNode root;
+    private List<TreeNode> list;
+    private int index;
 
+    public BSTIterator(TreeNode root) {
+        list = new LinkedList<>();
+        init(root);
+        this.root = root;
+    }
+
+    private void init(TreeNode root) {
+        if(root == null){
+            return;
+        }
+        init(root.left);
+        list.add(root);
+        init(root.right);
     }
 
     public int next() {
+        if(hasNext()){
+            TreeNode treeNode = list.get(index);
+            index ++;
+            return treeNode.val;
+        }
         return 0;
     }
 
     public boolean hasNext() {
-        return true;
+        if(index < list.size()){
+            return true;
+        }
+        return false;
     }
 }

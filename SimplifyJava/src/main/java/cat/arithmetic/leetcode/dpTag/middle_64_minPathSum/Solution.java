e876a097c8aa5c29fead7201ba03fa9c59b24331
高国兴 <nanyiniu76@gmail.com>
2021-05-27 20:27:31 +0800

    64. 最小路径和

---
+++ SimplifyJava/src/main/java/cat/arithmetic/leetcode/dpTag/middle_64_minPathSum/Solution.java
@@ -0,0 +1,64 @@
+package cat.arithmetic.leetcode.dpTag.middle_64_minPathSum;
+
+/**
+ * 64. 最小路径和
+ *
+ * 给定一个包含非负整数的 m x n 网格 grid ，
+ * 请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
+ *
+ * 说明：每次只能向下或者向右移动一步。
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：grid = [[1,3,1],[1,5,1],[4,2,1]]
+ * 输出：7
+ * 解释：因为路径 1→3→1→1→1 的总和最小。
+ * 示例 2：
+ *
+ * 输入：grid = [[1,2,3],[4,5,6]]
+ * 输出：12
+ *  
+ *
+ * 提示：
+ *
+ * m == grid.length
+ * n == grid[i].length
+ * 1 <= m, n <= 200
+ * 0 <= grid[i][j] <= 100
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/minimum-path-sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author gaoguoxing
+ * @version 1.0
+ * @date 2021-05-27
+ */
+public class Solution {
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        if (m == 0) {
+            return 0;
+        }
+        int n = grid[0].length;
+        if (n == 0) {
+            return 0;
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0) {
+                    if (j == 0) continue;
+                    else grid[i][j] = grid[i][j - 1] + grid[i][j];
+
+                } else if (j == 0) {
+                    if (i == 0) continue;
+                    else grid[i][j] = grid[i - 1][j] + grid[i][j];
+                } else {
+                    grid[i][j] = Math.min(grid[i][j - 1], grid[i - 1][j]) + grid[i][j];
+                }
+            }
+        }
+        return grid[m - 1][n - 1];
+    }
+}

66f863a22c9a511acd7f456af0a52b258ff11818
高国兴 <nanyiniu76@gmail.com>
2021-05-27 11:55:43 +0800

    53. 最大子序和

---
+++ SimplifyJava/src/main/java/cat/arithmetic/leetcode/dpTag/easy_53_maxSubArray/Solution.java
@@ -0,0 +1,84 @@
+package cat.arithmetic.leetcode.dpTag.easy_53_maxSubArray;
+
+/**
+ * 53. 最大子序和
+ *
+ * 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [-2,1,-3,4,-1,2,1,-5,4]
+ * 输出：6
+ * 解释：连续子数组 [4,-1,2,1] 的和最大，为 6 。
+ * 示例 2：
+ *
+ * 输入：nums = [1]
+ * 输出：1
+ * 示例 3：
+ *
+ * 输入：nums = [0]
+ * 输出：0
+ * 示例 4：
+ *
+ * 输入：nums = [-1]
+ * 输出：-1
+ * 示例 5：
+ *
+ * 输入：nums = [-100000]
+ * 输出：-100000
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= nums.length <= 3 * 104
+ * -105 <= nums[i] <= 105
+ *  
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/maximum-subarray
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author gaoguoxing
+ * @version 1.0
+ * @date 2021-05-27
+ */
+public class Solution {
+    public int maxSubArray2(int[] nums) {
+        int n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return nums[0];
+        }
+        int[][] dp = new int[n][n];
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = nums[i];
+            if(nums[i] > max){
+                max = nums[i];
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n - 1; j++) {
+                dp[i][j + 1] = dp[i][j] + nums[j+1];
+                if (dp[i][j + 1] > max) {
+                    max = dp[i][j + 1];
+                }
+            }
+        }
+        return max;
+    }
+
+    public int maxSubArray(int[] nums) {
+        int pre = 0, maxAns = nums[0];
+        for (int x : nums) {
+            pre = Math.max(pre + x, x);
+            maxAns = Math.max(maxAns, pre);
+        }
+        return maxAns;
+    }
+
+}
